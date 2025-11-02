aaca0dc4c82b0fb0e9bdec930295560126fe7db0
anvvvvvv <anvesha250705@gmail.com>
2025-11-02 21:52:17 +0530

    App.js

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FS_EXP_9.1 - Dockerized React App</h1>
+      <p>This React app is running inside a Docker container using a multi-stage build!</p>
+    </div>
+  );
+}
+
+export default App;
